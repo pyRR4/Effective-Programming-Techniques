c4d2ccb590267e179e86afd7a06d975f99c0bf53
Igor Podgórniak <igopood33@gmail.com>
2023-10-25 18:38:12 +0200

    sprawdzanie dlugosci inta przy operatorze=

--- TEP/Lista 2/CNumber/CNumber.cpp
+++ TEP/Lista 2/CNumber/CNumber.cpp
@@ -6,7 +6,7 @@ CNumber::CNumber()
 	i_length = DEFAULT_LENGTH;
 	pi_number = new int[i_length];
 	operator=(DEFAULT_NUM);
-	cout << "bezparam: " << toStr() << endl;
+	cout << "bezparam: " << endl;
 }
 
 CNumber::CNumber(int iNumber, int iLength) 
@@ -14,25 +14,39 @@ CNumber::CNumber(int iNumber, int iLength)
 	i_length = iLength;
 	pi_number = new int[i_length];
 	operator=(iNumber);
-	cout << "param: " << toStr() << endl;
+	cout << "param: " << endl;
 }
 
 CNumber::~CNumber()
 {
 	delete pi_number;
-	cout << "usuwam: " << toStr() << endl;
+	cout << "usuwam: " << endl;
 }
+
+string CNumber::sToStr()
+{
+	string retValue = "";
+	for (int i = 0; i < i_length; i++)
+		retValue += pi_number[i];
+	return retValue;
+}
+
+
+
 
 void CNumber::operator=(const int iValue)
 {
 	int x = iValue;
+	int int_length = intLength(x);
+	if (i_length < int_length)
+		reallocate(int_length);
 	for (int i = i_length; i >= 0; i--) {
 		pi_number[i] = x % 10;
 		x /= 10;
 	}
-	if (!(x < 10))
-		cout << "brak miejsca na ca³¹ liczbe" << endl; //ogarnij to
 }
+
+
 
 void CNumber::operator=(const CNumber& pcOther)
 {
@@ -40,8 +54,34 @@ void CNumber::operator=(const CNumber& pcOther)
 	i_length = pcOther.i_length;
 }
 
-string CNumber::toStr()
+void CNumber::reallocate(int iLength)
 {
+	i_length = iLength;
+	delete pi_number;
+	pi_number = new int[i_length];
+}
 
-	return "";
+
+int intLength(int iVal)
+{
+	int result = 0;
+	while (iVal != 0) {
+		result++;
+		iVal / 10;
+	}
+	return result;
+}
+
+
+int main() {
+	CNumber* num = new CNumber();
+	CNumber* num1 = new CNumber(5, 1);
+
+	*num = 3215;
+	*num1 = 2314439;
+
+	num->sToStr();
+	num1->sToStr();
+
+	return 0;
 }
--- TEP/Lista 2/CNumber/CNumber.h
+++ TEP/Lista 2/CNumber/CNumber.h
@@ -11,11 +11,13 @@ public:
 	CNumber();
 	CNumber(int iNumber, int iLength);
 	~CNumber();
+	string sToStr();
 	void operator= (const int iValue);
 	void operator= (const CNumber &pcOther);
-	string toStr();
+	void reallocate(int iLength);
 };
 
+static int intLength(int iVal);
 static const int DEFAULT_LENGTH = 5;
 static const int DEFAULT_NUM = 1;
 
